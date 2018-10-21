1813537d66532eba352af27b3ebc2510336f0527
tire.ho <tire.ho@kakaocorp.com>
2018-10-21 23:52:00 +0900

    view binding

--- src/main/java/com/googlecode/hotire/base/controller/BaseController.java
+++ src/main/java/com/googlecode/hotire/base/controller/BaseController.java
@@ -1,15 +1,22 @@
 package com.googlecode.hotire.base.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/base")
 public class BaseController {
-    @GetMapping()
+
+    @GetMapping({"/","/home"})
     public ModelAndView index() {
         return new ModelAndView("index");
+    }
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
+    @GetMapping("/hello")
+    public ModelAndView hello() {
+        return new ModelAndView("hello");
     }
 }
