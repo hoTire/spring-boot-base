b9b075115cbd4d31d46df5b4018fbb4829004f7b
tire.ho <tire.ho@kakaocorp.com>
2018-10-21 23:50:24 +0900

    add encoder

--- src/main/java/com/googlecode/hotire/base/config/SecurityConfig.java
+++ src/main/java/com/googlecode/hotire/base/config/SecurityConfig.java
@@ -1,30 +1,39 @@
 package com.googlecode.hotire.base.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
-                .antMatchers("/", "/base").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/","/home", "/base","/account/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
+            .formLogin()
                 .loginPage("/login")
                 .permitAll()
                 .and()
-                .logout()
+            .logout()
+                .logoutSuccessUrl("/home")
                 .permitAll();
     }
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/script/**", "/images/**", "/fonts/**", "/lib/**","/webjars/**", "/css/**","/js/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
