3009b9f1c409d6069b558251b4adc49f683ec1ef
hoTire <gngh0101@gmail.com>
2018-11-05 22:16:51 +0900

    aop order

---
+++ src/main/java/com/googlecode/hotire/base/config/AspectJConfig.java
@@ -0,0 +1,9 @@
+package com.googlecode.hotire.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectJConfig {
+}
