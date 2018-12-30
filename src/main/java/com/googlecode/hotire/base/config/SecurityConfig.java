fc98dfa4e37ed95ea72a9dffd6bf763cbb02ce74
hotire <gngh0101@gmail.com>
2018-12-30 17:52:40 +0900

    htmlunit

--- src/main/java/com/googlecode/hotire/base/config/SecurityConfig.java
+++ src/main/java/com/googlecode/hotire/base/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/hello")
                 .permitAll()
                 .and()
             .logout()
